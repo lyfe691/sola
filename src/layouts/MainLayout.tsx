8074caa9c571ad678a1a7cee4aea78509d0cf556
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2025-10-04 20:48:51 +0200

    chore: continue the migration

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,33 @@
+﻿/**
+ * Copyright (c) 2025 Yanis Sebastian Zürcher
+ *
+ * This file is part of a proprietary software project.
+ * Unauthorized copying, modification, or distribution is strictly prohibited.
+ * Refer to LICENSE for details or contact yanis.sebastian.zuercher@gmail.com for permissions.
+ */
+
+import React from "react";
+import Navigation from "@/components/Navigation";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+/**
+ * Mainlayout - layout for content pages with navigation
+ * used for all content pages including about, projects, skills, etc.
+ */
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <main className="min-h-[120vh] flex flex-col">
+      <div className="flex-1 p-5 sm:p-6 md:p-8 lg:p-12 pt-20 sm:pt-24 md:pt-20 flex flex-col">
+        <div className="max-w-7xl mx-auto w-full flex flex-col flex-1">
+          <Navigation />
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainLayout;
---
+++ src/layouts/ProjectLayout.tsx
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2025 Yanis Sebastian Zürcher
+ *
+ * This file is part of a proprietary software project.
+ * Unauthorized copying, modification, or distribution is strictly prohibited.
+ * Refer to LICENSE for details or contact yanis.sebastian.zuercher@gmail.com for permissions.
+ */
+
+import React from "react";
+
+interface ProjectLayoutProps {
+  children: React.ReactNode;
+}
+
+/**
+ * ProjectLayout - layout for project deep dives.
+ * Used for individual project pages like /projects/sola
+ */
+const ProjectLayout: React.FC<ProjectLayoutProps> = ({ children }) => {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <div className="flex-1 flex flex-col">{children}</div>
+    </main>
+  );
+};
+
+export default ProjectLayout;
