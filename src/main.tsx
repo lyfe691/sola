624a959264e68aa75dcd07a079444f357f11adef
lyfe691 <yanis.sebastian.zuercher@gmail.com>
2025-10-04 20:19:33 +0200

    chore: continue the migration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"; 
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+    <Analytics />
   </StrictMode>,
 )
